73bf5bed45dfa3d081e504c0a94011a2d6547b4d
ltanailov <ltanailov@gmail.com>
2026-07-08 19:48:57 +0300

    feat(app): hotfix gh-pages (2nd try)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/wotlk-guide/',
+    base: '/WotLK-Guide/',
     plugins: [react(), tailwindcss()],
 })
